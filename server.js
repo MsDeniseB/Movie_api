3b49562dad4e11efa0644b491a83abfd8847fe04
MsDeniseB <Denisebrazier2019@gmail.com>
2022-07-18 17:14:11 -0700

    2.2 update
    documentation now has description of api
    
    node modal files downloaded

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
 // My first Node test server is running on Port 8080.
 
 const http = require('http');
+const  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
 
-// url module
-
-let address = request.url;
-let q = new URL(addr, true);
-
-const  fs = require('fs'),
-  url = require('url');
-  
-fs.readFile('input.txt', (err, data) => {
-  if (err) {
-    throw err;
-  }
-// File content: ' + data.toString());
-});
-
-if (q.pathname.includes('documentation')) {
+if (request.url.includes('documentation')) {
     filePath = (__dirname + '/documentation.html');
   } else {
     filePath = 'index.html';
   }
+
+  fs.appendFile('log.txt', 'URL: ' + request.url + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
 
   fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -37,4 +30,10 @@ if (q.pathname.includes('documentation')) {
     response.write(data);
     response.end();
 
-  });listen(8080);
+  });
+
+  
+}).listen(8080);
+
+// url module
+
