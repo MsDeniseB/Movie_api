e159bfd6b8e2b35e83aa0c35eca84deb7669e526
MsDeniseB <Denisebrazier2019@gmail.com>
2022-08-18 16:42:12 -0700

    GetUser

--- index.js
+++ index.js
@@ -59,4 +59,28 @@ app.post('/users', (req, res) => {
     });
 });
 
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 app.listen(8080, ()=>console.log("Server started..."))
